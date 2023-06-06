4466bde52dc5e1feaf593aceba458cd6d6d9b5f6
Guilherme Henrique <gh.dourado77@gmail.com>
2023-06-06 14:04:33 -0300

    Initial commit

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,8 @@
+export default function Login(){
+  return(
+    <div>
+      <h1>Login</h1>
+      <a href="/sobre">Esqueci senha</a>
+    </div>
+  )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,36 @@
+import { lazy } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const LayoutMain = lazy(() => import("./layouts/LayoutMain"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const About = lazy(() => import("./pages/About/About"));
+const Login = lazy(() => import("./pages/Login/Login"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    //element: <LayoutMain />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "/sobre",
+        element: <About />,
+      }
+    ],
+  },
+]);
+
+
+export default function Routes() {
+  return (
+    <RouterProvider
+      router={router}
+      fallbackElement={<div>Carregando...</div>}
+    />
+  )
+}
