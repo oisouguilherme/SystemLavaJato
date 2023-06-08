b5402532fbb873c2ddffd3bdd418e6fe2b5972c7
Guilherme Henrique <gh.dourado77@gmail.com>
2023-06-08 11:19:18 -0300

    Update index.tsx

--- src/pages/Custo/Modal/index.tsx
+++ src/pages/Custo/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import IconThreepoints from "../../../assets/images/threePoints.svg";
 import { Link } from 'react-router-dom';
 
